6c5d4873e4175d4435ccfe04b8e646207324643f
AshW165 <148209618+AshW165@users.noreply.github.com>
2025-11-28 13:34:08 +0530

    First push
    COMMMENT

--- brick_game.c
+++ brick_game.c
@@ -8,6 +8,8 @@
 #include "input.h"
 #include "sound.h"  // <-- sound integration (PB4 beeps)
 
+/*ABCDEF*/
+
 /************************************************************
  * BRICK BREAKER – MULTI-LEVEL ENGINE (WITH SOUND)
  ************************************************************/
